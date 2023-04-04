d4a991ef72dcc11eba75d05e4b0fb31a87fe4880
jomari abejo <jomariabejo@gmail.com>
2023-04-04 15:40:13 +0800

    added comments

--- Stocks.java
+++ Stocks.java
@@ -2,118 +2,185 @@ import java.util.Arrays;
 
 public class Stocks
 {
+
   public static void main (String[] args)
   {
-    Find find = new Find ();
-    Sort sort = new Sort ();
-    Create create = new Create ();
-    Delete delete = new Delete ();
 
-    String[] Brand = {
-      "Honda", "Kawasaki", "Kymco", "Yamaha", "Suzuki",
-    };
-    /** Print all brand */
+    /*
+     *  --Demo Execution
+     */
+
+    // Initialize the Brands of MotorPH
+    String[] Brand
+            = { "Honda", "Kawasaki", "Kymco", "Yamaha", "Suzuki", "Samsung" };
+
+    System.out.println ("PRINT ALL BRAND");
+    for (String getBrandName : Brand)
+    {
+      System.out.println (getBrandName);
+    }
+
+    System.out.println ("\n\n\n"
+            + "Let's try to find the Harley-Davidson");
+    String findResult = Find.findBrandByName ("Harley-Davidson", Brand);
+    String finalFindResult
+            = findResult.equals ("Harley-Davidson") ? "FOUND" : "NOT FOUND";
+    System.out.println (finalFindResult);
+
+    System.out.println (
+            "Let's add it to the array because it isn't there.\n\n\n");
+
+    String[] harleydavidsonHasBeenAdded
+            = Create.insertBrandByName ("Harley-Davidson", Brand);
+    System.out.println ("Let's find out if Harley-Davidson is found");
+    for (String isItHarleyDavidson : harleydavidsonHasBeenAdded)
+    {
+      int harleyDavidsonIndex = 0;
+      if (isItHarleyDavidson.equals ("Harley-Davidson"))
+      {
+        System.out.println ("FOUND "+ "😲");
+        break;
+      }
+      else
+        System.out.println ("NOTFOUND😔");
+      harleyDavidsonIndex++;
+    }
+
+
+    // BreakLine
+    System.out.println ("\n\n");
+
+    // It was found🙆!
+    System.out.println (
+            "Let us now try to delete \"Samsung\" to the array because Samsung is not a Motorcycle Brand");
+    String[] samsungRemovedArray
+            = Delete.deleteBrandbyName ("Samsung", harleydavidsonHasBeenAdded);
+    System.out.print ("\n Check if Samsung removed : ");
+
+    for (String get : samsungRemovedArray)
+    {
+      System.out.print (get + ",");
+    }
+
+    System.out.println("\n\n\n");
+
+
   }
-}
 
-class Find
-{
-  public String
-  findBrandByName (String brandname, String[] brandsArray)
+  class Find
   {
-    for (String get : brandsArray)
+    public static String
+    findBrandByName (String brandname, String[] brandsArray)
+    {
+      for (String get : brandsArray)
       {
         if (get.equals (brandname))
-          {
-            return get;
-          }
+        {
+          return get;
+        }
       }
-    return "Not Found";
+      return "Not Found";
+    }
   }
-}
 
-class Sort
-{
-  public String[] sortBrandByName (String[] brandsArray)
+  class Sort
   {
-    //  Sorted the array via Arrays.sort
-    Arrays.sort (brandsArray);
-    String[] sorted = new String[brandsArray.length];
-    int index = 0;
-    for (String get : brandsArray)
+    public static String[] sortBrandByName (String[] brandsArray)
+    {
+      //  Sorted the array via Arrays.sort
+      Arrays.sort (brandsArray);
+      String[] sorted = new String[brandsArray.length];
+      int index = 0;
+      for (String get : brandsArray)
       {
         sorted[index] = get;
         index++;
       }
-    return sorted;
+      return sorted;
+    }
   }
-}
-
-class Create
-{
-  public String[] insertBrandByName (String carBrand, String[] brandsArray)
+  /*
+   *  Creates an element from the input array if it matches the specified String
+   *  @param deleteThisBrand - The string value to be removed from the input
+   * array
+   *  @param brandsArray  - The input array of string values
+   *  @return - A new string array with the specified value removed, or the
+   * original array if the value is not found
+   *
+   */
+  class Create
   {
+    /*
+     *  Duplicates the old Array and increment once the index to put
+     */
+    public static String[] insertBrandByName (String brandName,
+                                              String[] brandsArray)
+    {
 
-    int incrementThePreviousBrandsArrayLength = brandsArray.length + 1;
-    String[] updatedBrandsArray
-        = new String[incrementThePreviousBrandsArrayLength];
-    int index = 0;
-    while (index < incrementThePreviousBrandsArrayLength)
+      int incrementThePreviousBrandsArrayLength = brandsArray.length + 1;
+      String[] updatedBrandsArray
+              = new String[incrementThePreviousBrandsArrayLength];
+      int index = 0;
+      while (index < incrementThePreviousBrandsArrayLength)
       {
-
         if (index + 1 == incrementThePreviousBrandsArrayLength)
-          {
-            updatedBrandsArray[index] = carBrand;
-            break;
-          }
+        {
+          updatedBrandsArray[index] = brandName;
+          break;
+        }
 
         updatedBrandsArray[index] = brandsArray[index];
         index++;
       }
-    return updatedBrandsArray;
+      return updatedBrandsArray;
+    }
   }
-}
 
-// TODO: 4/4/2023 DELETE METHOD IS DON'T GIVE RESULT PROPERLY
-class Delete
-{
-  public String[] deleteBrandbyName (String deleteThisBrand,
-                                     String[] brandsArray)
+
+  class Delete
   {
-    int index = 0;
-    int removeOneIndex = brandsArray.length - 1;
+    /*
+     *  Deletes an element from the input array if it matches the specified String
+     *  @param deleteThisBrand - The string value to be removed from the input array
+     *  @param brandsArray  - The input array of string values
+     *  @return - A new string array with the specified value removed, or the
+     * original array if the value is not found
+     *
+     */
+    public static String[] deleteBrandbyName (String deleteItem,
+                                              String[] brandsArray)
+    {
+      int i = 0; // Initialize the loop counter variable to 0
+      int decrementedBrandArrayLength
+              = brandsArray.length - 1; // New Length of the Updated Array
 
-    for (String getValue : brandsArray)
-      {
-        if (deleteThisBrand.equals (getValue))
-          {
-            brandsArray[index] = "deleteThisBrand";
-          }
-        index++;
-      }
-    // set zero to reuse the index.
-    index = 0;
-    String[] updatedBrandsArray = new String[removeOneIndex];
-    System.out.println ("The length of updatedbrands array is "
-                        + updatedBrandsArray.length);
+      String[] newVersionArray
+              = new String[decrementedBrandArrayLength]; // each element of the
+      // array is initialised to
+      // null
 
-    while (index < removeOneIndex)
+      // Check if getvalue equals to @deleteItem
+      for (String getValue : brandsArray)
       {
-        if (brandsArray[index].equals ("deleteThisBrand"))
-          {
-            index++;
-            //                System.out.println("@if"+index + " = " +
-            //                brandsArray[index]);
-          }
-        else
-          {
-            updatedBrandsArray[index] = brandsArray[index];
-            index++;
-            //                System.out.println("@else"+index  + " = " +
-            //                brandsArray[index]);
-          }
-        //            System.out.println("Null @ " + brandsArray[index]);
+        if (deleteItem.equals (getValue))
+        {
+          brandsArray[i] = "deleteItem";
+        }
+        i++;
       }
-    return updatedBrandsArray;
+      i = 0;     // set zero to reuse the counter
+      int j = 0; //  counter for newVersionArray. This helps to avoid an out
+      //  of bounds exception.
+      while (i < brandsArray.length)
+      {
+        if (!brandsArray[i].equals ("deleteItem"))
+        {
+          newVersionArray[j] = brandsArray[i];
+          j++;
+        }
+        i++;
+      }
+      return newVersionArray;
+    }
   }
 }
